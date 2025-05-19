cff5a2d38ea9b784bf930f7314f7c651ccff1f85
Toxic122 <89800054+Toxic122@users.noreply.github.com>
2025-05-20 00:05:58 +0200

    GetClients_V1
    Kontroler pobierający klientów do frontu według wprowadzonego imienia i nazwiska klienta

---
+++ Models/ConsultantScreen/ClientSearchRequest.cs
@@ -0,0 +1,8 @@
+﻿//Tylko na potrzeby testów jeśli zmienilibyśmy metodę na post
+namespace ISP2.Models.ConsultantScreen
+{ 
+    public class ClientSearchRequest
+    {
+        public string? FullName { get; set; } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,18 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using ISP2.Data;
 using ISP2.Repositories.LoginScreen;
 using ISP2.Services.LoginScreen;
+using ISP2.Repositories.ConsultantScreen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Dodaj EF Core z SQL Server
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Dodaj repozytoria i serwisy
+
 builder.Services.AddScoped<IUserRepository, EfUserRepository>();
 builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IConsultantRepository, EfConsultantRepository>();
 
-// Dodaj Swaggera
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,7 +22,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// U¿yj Swaggera tylko w trybie developerskim
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
